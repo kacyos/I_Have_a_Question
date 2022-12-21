0e61c4d8d8cf2cc6d82dea87f7b3a5ac666c7633
Cacio de Castro <cacio.castro@gmail.com>
2022-12-21 19:13:53 -0300

    Aplicação Node de Perguntas e Respostas

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", {});
+});
+
+app.get("/question", (req, res) => {
+  res.render("question", {});
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
